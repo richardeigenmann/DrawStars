62616b36a1b1a8c2767503ce7d233601964417ac
Richard Eigenmann <richard.eigenmann@gmail.com>
2015-02-08 14:54:08 +0100

    Improve Presentation packaging script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,25 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                    {expand: true, flatten: true, src: ['bower_components/reveal.js/css/theme/league.css'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/reveal.js/lib/font/league-gothic/*'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/highlightjs/styles/zenburn.css'], dest: 'public_html/build/', filter: 'isFile'},
+                ],
+            },
+        },
         bower_concat: {
             all: {
                 dest: 'public_html/build/presentation-concat.js',
                 cssDest: 'public_html/build/presentation.css',
                 include: ['reveal.js'],
                 mainFiles: {
-                    'reveal.js': ['js/reveal.js', 'lib/js/head.min.js','lib/js/classList.js',
+                    'reveal.js': ['js/reveal.js', 'lib/js/head.min.js', 'lib/js/classList.js',
                         'plugin/highlight/highlight.js',
-                        'css/reveal.css', 'lib/css/zenburn.css']
+                        'css/reveal.css', 
+                        'bower_components/reveal.js/lib/font/league-gothic/league-gothic.css']
                 }
             }
         },
@@ -30,5 +40,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.registerTask('default', ['bower_concat', 'uglify:bower']);
 }
--- README.md
+++ README.md
@@ -4,3 +4,8 @@ DrawStars
 An AngularJS directive to draw stars on a webpage
 http://richardeigenmann.github.io/DrawStars/presentation.html
 
+Reminder for self how to build the presentation:
+npm install grunt -g
+grunt
+copy files from public_html/build to branch gh-pages
+
