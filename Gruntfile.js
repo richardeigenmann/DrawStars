4b44db2bd7ccad64207d3bd18225e53673254500
Richard Eigenmann <richard.eigenmann@gmail.com>
2015-02-08 15:35:02 +0100

    Improving Build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,17 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 files: [
+                    {expand: true, flatten: true, src: ['public_html/presentation.html'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['public_html/drawStars.js'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['public_html/drawStarsApp.js'], dest: 'public_html/build/', filter: 'isFile'},
                     {expand: true, flatten: true, src: ['bower_components/reveal.js/css/theme/league.css'], dest: 'public_html/build/', filter: 'isFile'},
                     {expand: true, flatten: true, src: ['bower_components/reveal.js/lib/font/league-gothic/*'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/highlightjs/highlight.pack.js'], dest: 'public_html/build/', filter: 'isFile'},
                     {expand: true, flatten: true, src: ['bower_components/highlightjs/styles/zenburn.css'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/reveal.js/plugin/zoom-js/zoom.js'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/reveal.js/plugin/notes/notes.js'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/reveal.js/plugin/remotes/remotes.js'], dest: 'public_html/build/', filter: 'isFile'},
+                    {expand: true, flatten: true, src: ['bower_components/JSColor/arrow.gif', 'bower_components/JSColor/cross.gif', 'bower_components/JSColor/hs.png', 'bower_components/JSColor/hv.png'], dest: 'public_html/build/jscolor/', filter: 'isFile'},
                 ],
             },
         },
@@ -14,12 +22,11 @@ module.exports = function (grunt) {
             all: {
                 dest: 'public_html/build/presentation-concat.js',
                 cssDest: 'public_html/build/presentation.css',
-                include: ['reveal.js'],
+                include: ['reveal.js', 'JSColor'],
                 mainFiles: {
                     'reveal.js': ['js/reveal.js', 'lib/js/head.min.js', 'lib/js/classList.js',
                         'plugin/highlight/highlight.js',
-                        'css/reveal.css', 
-                        'bower_components/reveal.js/lib/font/league-gothic/league-gothic.css']
+                        'css/reveal.css']
                 }
             }
         },
@@ -41,5 +48,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['bower_concat', 'uglify:bower']);
+    grunt.registerTask('default', ['copy', 'bower_concat', 'uglify:bower']);
 }
